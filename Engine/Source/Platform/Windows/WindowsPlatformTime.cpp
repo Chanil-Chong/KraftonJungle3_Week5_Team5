82522554be3099e9e4499c0130f75ab715b3e241
keonwookang0914 <kwkang0914@gmail.com>
2026-04-03 22:13:51 +0900

    feat: 발제 내용에 있는 퍼포먼스 측정용 클래스 추가

---
+++ Engine/Source/Platform/Windows/WindowsPlatformTime.cpp
@@ -0,0 +1,1 @@
+#include "WindowsPlatformTime.h"
---
+++ Engine/Source/Platform/Windows/WindowsPlatformTime.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "CoreMinimal.h"
+#include <winnt.h>
+#include <profileapi.h>
+
+class FWindowsPlatformTime
+{
+public:
+	static double GSecondsPerCycle; // 0
+	static bool bInitialized; // false
+
+	static void InitTiming()
+	{
+		if (!bInitialized)
+		{
+			bInitialized = true;
+
+			double Frequency = (double)GetFrequency();
+			if (Frequency <= 0.0)
+			{
+				Frequency = 1.0;
+			}
+
+			GSecondsPerCycle = 1.0 / Frequency;
+		}
+	}
+	static float GetSecondsPerCycle()
+	{
+		if (!bInitialized)
+		{
+			InitTiming();
+		}
+		return (float)GSecondsPerCycle;
+	}
+	static uint64 GetFrequency()
+	{
+		LARGE_INTEGER Frequency;
+		QueryPerformanceFrequency(&Frequency);
+		return Frequency.QuadPart;
+	}
+	static double ToMilliseconds(uint64 CycleDiff)
+	{
+		double Ms = static_cast<double>(CycleDiff)
+			* GetSecondsPerCycle()
+			* 1000.0;
+
+		return Ms;
+	}
+
+	static uint64 Cycles64()
+	{
+		LARGE_INTEGER CycleCount;
+		QueryPerformanceCounter(&CycleCount);
+		return (uint64)CycleCount.QuadPart;
+	}
+};
